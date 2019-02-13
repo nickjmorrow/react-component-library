ed488bcbc695b2b3d03dd87015977f09dbf9d092
Nicholas Morrow <njmorrow95@gmail.com>
2019-02-12 21:35:21 -0500

    Added theme support to styleConstants

--- src/styleConstants.ts
+++ src/styleConstants.ts
@@ -1,36 +1,81 @@
-// TODO: be able to override this at an app level (e.g. specify different defaults for primary, secondary, etc)
-export const colors = {
-  white: "rgb(255, 255, 255)",
-  primary: "rgb( 48, 70,161)",
-  primaryDarkest: "rgb(  4, 15, 61)",
-  primaryDark: "rgb( 19, 38,113)",
-  primaryLight: "rgb( 96,115,194)",
-  primaryLightest: "rgb(189,198,234)",
-  tertiary: "rgb(185, 37,130)",
-  tertiaryDarkest: "rgb( 70,  0, 44)",
-  tertiaryDark: "rgb(130,  7, 84)",
-  tertiaryLight: "rgb(213, 91,167)",
-  tertiaryLightest: "rgb(241,188,221)",
-  secondary: "rgb(237,180, 47)",
-  secondaryDarkest: "rgb( 90, 63,  0)",
-  secondaryDark: "rgb(167,120,  9)",
-  secondaryLight: "rgb(255,211,109)",
-  secondaryLightest: "rgb(255,238,199)",
-  darkGray: "hsla(0, 0%, 0%, 0.8)",
-  gray: "hsla(0, 0%, 0%, 0.6)",
-  lightGray: "hsla(0, 0%, 0%, 0.2)",
-  lighterGray: "hsla(0, 0%, 0%, 0.1)",
-  transparent: "transparent",
-  red: "#E55",
-  redLight: "#ED8D8D",
-  redDark: "#C14747"
+import * as React from "react";
+import * as deepmergeProxy from "deepmerge";
+import { DeepRequired, DeepPartial } from "ts-essentials";
+const deepmerge: typeof deepmergeProxy =
+  (deepmergeProxy as any).default || deepmergeProxy;
+
+const defaultPrimaryPaletteDescriptor = {
+  hue: 220, // hsl(220, 100%, 50%)
+  middleLightness: 50,
+  lightnessIncrement: 10,
+  saturation: 55
 };
 
-export const googleColors = {
-  googleRed: "#D95434",
-  googleRedLight: "#D8836F",
-  googleRedDark: "#AE4229"
+const defaultSecondaryPaletteDescriptor = {
+  ...defaultPrimaryPaletteDescriptor,
+  hue: 160 // hsl(160, 100%, 50%)
 };
+
+const defaultGrayPaletteDescriptor = {
+  ...defaultPrimaryPaletteDescriptor,
+  saturation: 0
+};
+
+const defaultErrorPaletteDescriptor = {
+  ...defaultPrimaryPaletteDescriptor,
+  hue: 50 // hsl(10, 100%, 50%)
+};
+
+interface ColorShades {
+  lightest: string;
+  lighter: string;
+  light: string;
+  main: string;
+  dark: string;
+  darker: string;
+  darkest: string;
+}
+
+interface ShadesDescriptor {
+  hue?: number;
+  middleLightness?: number;
+  lightnessIncrement?: number;
+  saturation?: number;
+}
+
+export const generateColorShades = (
+  paletteDescriptor: ShadesDescriptor
+): ColorShades => {
+  const {
+    hue: h = 220,
+    middleLightness: l = 50,
+    saturation: s = 50,
+    lightnessIncrement: li = 10
+  } = paletteDescriptor;
+  return {
+    lightest: `hsl(${h}, ${s}%, ${l + 3 * li}%)`,
+    lighter: `hsl(${h}, ${s}%, ${l + 2 * li}%)`,
+    light: `hsl(${h}, ${s}%, ${l + li}%)`,
+    main: `hsl(${h}, ${s}%, ${l}%)`,
+    dark: `hsl(${h}, ${s}%, ${l - li}%)`,
+    darker: `hsl(${h}, ${s}%, ${l - 2 * li}%)`,
+    darkest: `hsl(${h}, ${s}%, ${l - 3 * li}%)`
+  };
+};
+
+export const colors = {
+  white: "rgb(255, 255, 255)",
+  transparent: "transparent",
+  primary: generateColorShades(defaultPrimaryPaletteDescriptor),
+  secondary: generateColorShades(defaultSecondaryPaletteDescriptor),
+  gray: generateColorShades(defaultGrayPaletteDescriptor),
+  red: generateColorShades(defaultErrorPaletteDescriptor)
+};
+
+export const googleColors = generateColorShades({
+  hue: 10,
+  saturation: 70
+});
 
 export enum borderRadius {
   default = "4px",
@@ -56,15 +101,34 @@ export const verticalMargin = {
 };
 
 export const boxShadow = {
-  default: `0px 2px 6px ${colors.lightGray}`,
-  light: `0px 2px 6px ${colors.primaryLight}`
+  default: `0px 2px 6px ${colors.gray.main}`,
+  light: `0px 2px 6px ${colors.primary.light}`
 };
 
 export const border = {
-  default: `1px solid ${colors.primary}`,
-  bold: `3px solid ${colors.primary}`
+  default: `1px solid ${colors.primary.main}`,
+  bold: `3px solid ${colors.primary.main}`
 };
 
 export const horizontalSpacing = {
   default: "1.5rem"
 };
+
+export const defaultTheme = {
+  colors,
+  transitions,
+  borderRadius,
+  boxShadow
+};
+
+type OptionalTheme = DeepPartial<typeof defaultTheme>;
+
+export const ThemeContext = React.createContext(defaultTheme as OptionalTheme);
+
+// merge theme with defaultTheme
+export const getStyles = (theme: DeepPartial<typeof defaultTheme>) =>
+  theme
+    ? deepmerge<typeof defaultTheme>(defaultTheme, theme as DeepRequired<
+        typeof theme
+      >)
+    : defaultTheme;
