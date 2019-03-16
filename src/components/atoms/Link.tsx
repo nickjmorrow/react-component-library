87110a708f95b115925ab8baa997ab8fde10c2fe
Nicholas Morrow <njmorrow95@gmail.com>
2019-03-16 17:20:23 -0400

    minor changes to Link

--- src/components/atoms/Link.tsx
+++ src/components/atoms/Link.tsx
@@ -1,22 +1,46 @@
 import * as React from "react";
 import { Link as RouterLink } from "react-router-dom";
-import { getFormattedTextNode } from "./typography";
+import { Typography } from "./typography";
+import { ColorVariant, StyleVariant } from "./types";
+import { GetComponentProps } from "~/typeUtilities";
 
 export const Link: React.SFC<{
   route: string;
-  children: React.ReactNode;
+  children: React.ReactText;
   style?: React.CSSProperties;
-}> = ({ children, route, style }) => {
-  const defaultLinkStyle = {
-    textDecoration: "none"
-  };
-  const customStyle = { ...defaultLinkStyle, style };
+  styleVariant: StyleVariant;
+  typographyProps?: GetComponentProps<typeof Typography>;
+}> = ({
+  children,
+  route,
+  style,
+  styleVariant = "primary",
+  typographyProps
+}) => {
+  const customStyle = { ...defaultLinkStyle, ...style };
   return (
     <RouterLink to={route} style={customStyle}>
-      {getFormattedTextNode(children, {
-        colorVariant: "core",
-        style: { textDecoration: "none" }
-      })}
+      <Typography
+        colorVariant={getColorVariant(styleVariant)}
+        {...typographyProps}>
+        {children}
+      </Typography>
     </RouterLink>
   );
+};
+
+const defaultLinkStyle = {
+  textDecoration: "none",
+  display: "flex",
+  alignItems: "center"
+};
+
+const getColorVariant = (styleVariant: StyleVariant): ColorVariant => {
+  switch (styleVariant) {
+    case "primary":
+      return "core";
+    case "secondary":
+    case "tertiary":
+      return "primaryDark";
+  }
 };
